60ad5aa512652b18fd06bd98266cf36d1edc73dd
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-10-02 15:44:54 +0200

    make controller fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -750,16 +750,16 @@ class UserController extends Controller
     }
 
 
-    function pkcs5_unpad($text) {
-        $pad = ord($text{strlen($text)-1});
-        if ($pad > strlen($text)) {
-            return false;
-        }
-        if (strspn($text, chr($pad), strlen($text) - $pad) != $pad) {
-            return false;
-        }
-        return substr($text, 0, -1 * $pad);
-    }
+//    function pkcs5_unpad($text) {
+//        $pad = ord($text{strlen($text)-1});
+//        if ($pad > strlen($text)) {
+//            return false;
+//        }
+//        if (strspn($text, chr($pad), strlen($text) - $pad) != $pad) {
+//            return false;
+//        }
+//        return substr($text, 0, -1 * $pad);
+//    }
 
     // excute pay
     public function excute_pay(Request $request) {
