868b0a8b61bfbd47e7936baa2fc446b84a2e7235
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-10-06 14:50:00 +0200

    make pin

--- app/Product.php
+++ app/Product.php
@@ -10,7 +10,7 @@ class Product extends Model
     protected $appends = array('views');
     protected $fillable = ['title','description', 'price','category_id','sub_category_id','sub_category_two_id','expire_special_date',
         'sub_category_three_id','sub_category_four_id','user_id', 'type','publication_date','re_post_date','is_special',
-        'views', 'offer', 'status', 'expiry_date','main_image','expire_pin_date','created_at','plan_id','publish','sub_category_five_id','deleted'];
+        'views', 'offer', 'status', 'expiry_date','main_image','expire_pin_date','pin','created_at','plan_id','publish','sub_category_five_id','deleted'];
 
     public function category() {
         return $this->belongsTo('App\Category', 'category_id');
